a5c43ad9ce7aadf47732dfbe90c943bc746b9751
Cliffton S <clifftonsoenarto8@gmail.com>
2020-12-18 10:54:44 +0700

    commit praktikum 14

---
+++ PraktikumWeek14/PraktikumWeek14/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace PraktikumWeek14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblinput = new System.Windows.Forms.Label();
+            this.txtboxinput = new System.Windows.Forms.TextBox();
+            this.btnreverse = new System.Windows.Forms.Button();
+            this.btnurutkan = new System.Windows.Forms.Button();
+            this.lbloutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblinput
+            // 
+            this.lblinput.AutoSize = true;
+            this.lblinput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblinput.Location = new System.Drawing.Point(40, 63);
+            this.lblinput.Name = "lblinput";
+            this.lblinput.Size = new System.Drawing.Size(39, 17);
+            this.lblinput.TabIndex = 0;
+            this.lblinput.Text = "Input";
+            // 
+            // txtboxinput
+            // 
+            this.txtboxinput.Location = new System.Drawing.Point(43, 83);
+            this.txtboxinput.Name = "txtboxinput";
+            this.txtboxinput.Size = new System.Drawing.Size(187, 20);
+            this.txtboxinput.TabIndex = 1;
+            // 
+            // btnreverse
+            // 
+            this.btnreverse.Location = new System.Drawing.Point(43, 143);
+            this.btnreverse.Name = "btnreverse";
+            this.btnreverse.Size = new System.Drawing.Size(88, 23);
+            this.btnreverse.TabIndex = 2;
+            this.btnreverse.Text = "REVERSE";
+            this.btnreverse.UseVisualStyleBackColor = true;
+            this.btnreverse.Click += new System.EventHandler(this.btnreverse_Click);
+            // 
+            // btnurutkan
+            // 
+            this.btnurutkan.Location = new System.Drawing.Point(144, 143);
+            this.btnurutkan.Name = "btnurutkan";
+            this.btnurutkan.Size = new System.Drawing.Size(86, 23);
+            this.btnurutkan.TabIndex = 3;
+            this.btnurutkan.Text = "URUTKAN";
+            this.btnurutkan.UseVisualStyleBackColor = true;
+            this.btnurutkan.Click += new System.EventHandler(this.btnurutkan_Click);
+            // 
+            // lbloutput
+            // 
+            this.lbloutput.AutoSize = true;
+            this.lbloutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbloutput.Location = new System.Drawing.Point(40, 235);
+            this.lbloutput.Name = "lbloutput";
+            this.lbloutput.Size = new System.Drawing.Size(51, 17);
+            this.lbloutput.TabIndex = 4;
+            this.lbloutput.Text = "Output";
+            this.lbloutput.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 366);
+            this.Controls.Add(this.lbloutput);
+            this.Controls.Add(this.btnurutkan);
+            this.Controls.Add(this.btnreverse);
+            this.Controls.Add(this.txtboxinput);
+            this.Controls.Add(this.lblinput);
+            this.Name = "Form1";
+            this.Text = "String";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblinput;
+        private System.Windows.Forms.TextBox txtboxinput;
+        private System.Windows.Forms.Button btnreverse;
+        private System.Windows.Forms.Button btnurutkan;
+        private System.Windows.Forms.Label lbloutput;
+    }
+}
+
---
+++ PraktikumWeek14/PraktikumWeek14/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PraktikumWeek14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnreverse_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "")
+            {
+                MessageBox.Show("Tolong isi textbox terlebih dahulu!");
+            }
+            else
+            {
+                string s1, s2;
+                s2 = "";
+                s1 = txtboxinput.Text;
+                for (int i = s1.Length-1; i >= 0; i--)
+                {
+                    s2 = s2 + s1[i];
+                }
+                lbloutput.Text = s2;
+                txtboxinput.Text = "";
+            }
+        }
+
+        private void btnurutkan_Click(object sender, EventArgs e)
+        {
+            if (txtboxinput.Text == "")
+            {
+                MessageBox.Show("Tolong isi textbox terlebih dahulu!");
+            }
+            else
+            {
+                string s1;
+                char s3;
+                s1 = txtboxinput.Text;
+                char[] index = s1.ToCharArray();
+                for (int i = 0; i <= s1.Length - 1; i++)
+                {
+                    for(int j = 0; j < i; j++)
+                    {
+                        if (index[i] < index[j])
+                        {
+                            s3 = index[i];
+                            index[i] = index[j];
+                            index[j] = s3;
+                        }
+                    }
+                }
+                string hasil = new string(index);
+                lbloutput.Text = hasil;
+
+
+                txtboxinput.Text = "";
+            }
+        }
+    }
+}
